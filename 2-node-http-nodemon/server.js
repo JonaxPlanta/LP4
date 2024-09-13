663ab78fd447490265c3711b312b57e716cf1b6e
JonaxPlanta <joao.flores3@estudante.ifms.edu.br>
2024-09-13 11:46:22 -0300

    Editing the code.

--- 2-node-http-nodemon/server.js
+++ 2-node-http-nodemon/server.js
@@ -6,18 +6,20 @@ const hostname = '127.0.0.1'
 // Criando porta para selecionar uma porta do computador
 const port = 3000;
 
-// função anônima para criar servidor da biblioteca http
-// parâmetros requisição e resposta são padrâo
+// Função anônima para criar servidor da biblioteca http
+// Os parâmetros requisição (quando o usuário faz uma requisição) e
+// resposta (quando o servidor está dando uma resposta ao) são padrâo
 const server = http.createServer((require, respost) => {
-    // código de status tem que ser igual a 200 para estar 200
+    // Código de status tem que ser igual a 200 para estar tudo certo
     respost.statusCode = 200;
-    // configurando o cabeçalho (tipos de conteúdos, etc...)
+    // Configurando o cabeçalho da página (tipo de conteúdo, texto do tipo plano e utf-8 para mapa de caracteres)
     respost.setHeader('Content-Type', 'text/plan; charset=utf-8');
-    // escreve uma frase no servidor
+    // Escreve uma frase no servidor, na página
     respost.write('Olá! Este é um servidor HTTP.');
-    // terminando resposta
+    // Terminando resposta do servidor
     respost.end();
 });
 
-// executar o servidor (criando uma escuta, uma espera para alguém que digitar o endereço 'localhost')
+// Executar o servidor (criando uma escuta, uma espera para alguém que entrar no endereço 'localhost')
+// com parâmetros porta de acesso do servidor, endereço IP do servidor e mensagem para o terminal
 server.listen(port, hostname, console.log('O servidor está rodando...'));
