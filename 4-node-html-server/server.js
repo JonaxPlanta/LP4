a48c6db1a2a4d090e207d3dbdf51304648733b0e
JonaxPlanta <joao.flores3@estudante.ifms.edu.br>
2024-10-04 12:21:44 -0300

    Editing...

--- 4-node-html-server/server.js
+++ 4-node-html-server/server.js
@@ -1,17 +1,28 @@
-const http = require('http');
+// Importing the HTTP module from HTTP library 
+const http = require("http");
+// Importing the fileSystem module from FS library 
 const fileSystem = require('fs');
 
-const localhost = "127.0.0.1";
+// Creating a 'localhost' with the IP address from self machine
+const hostname = '127.0.0.1'
+// Creating an access port to select which computer port the server will run on
 const port = 3000;
+
 
 // Settings of server
 const server = http.createServer((require, response) => {
     // Settings of header
     response.writeHead(200, { "Content-type" : "text/html; charset=utf-8" });
 
+    // Using FS library to read a file from a local file 
     const htmlFile = fileSystem.readFileSync("./index.html") 
 
+    // Response with the local file read by the FS
     response.end(htmlFile);
 });
 
-server.listen(port, localhost, console.log("Server running..."))
+// Runs the server (creates a listen, a wait anyone who wants to enter the server address)
+// with parameterss: server access port, server IP address and a message in terminal
+server.listen(port, hostname, console.log('The server is running...'));
+
+// "node --watch 'server.js'"
--- 5-api-server-json/server.js
+++ 5-api-server-json/server.js
@@ -1,7 +1,11 @@
-const http = require('http');
+// Importing the HTTP module from HTTP library 
+const http = require("http");
 
-const localhost = "127.0.0.1";
+// Creating a 'localhost' with the IP address from self machine
+const hostname = '127.0.0.1'
+// Creating an access port to select which computer port the server will run on
 const port = 3000;
+
 
 // Settings of server
 const server = http.createServer((require, response) => {
@@ -15,7 +19,12 @@ const server = http.createServer((require, response) => {
     // Settings of header
     response.writeHead(200, { "Content-type" : "application/json; charset=utf-8" });
 
+    // do a response with a JSON file type
     response.end(JSON.stringify(person));
 });
 
-server.listen(port, localhost, console.log("Server running..."))
+// Runs the server (creates a listen, a wait anyone who wants to enter the server address)
+// with parameterss: server access port, server IP address and a message in terminal
+server.listen(port, hostname, console.log('The server is running...'));
+
+// "node --watch 'server.js'"
