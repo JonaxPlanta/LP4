96e3e4fb606ad80b93206c910dcb78437931fd5f
JonaxPlanta <joao.flores3@estudante.ifms.edu.br>
2024-10-04 12:39:05 -0300

    Editing...

--- 6-server-routes/server.js
+++ 6-server-routes/server.js
@@ -6,21 +6,23 @@ const hostname = '127.0.0.1'
 // Creating an access port to select which computer port the server will run on
 const port = 3000;
 
-
 // Settings of server
 const server = http.createServer((require, response) => {
-    // JSon objects
-    const person = {
-        name : "Jason",
-        email: "jason@json.com",
-        telephone: "1191-934-0394"
-    }
-
     // Settings of header
-    response.writeHead(200, { "Content-type" : "application/json; charset=utf-8" });
+    response.writeHead(200, { "Content-type" : "text/json; charset=utf-8" });
 
-    // Response with a JSON file type
-    response.end(JSON.stringify(person));
+    // Requires URL from user 
+    const url = require.url;
+
+    // Manipulating URL
+    if (url === "/") {
+        response.write("You are in the Website main root.") // Write a message
+        response.end(); // End the response
+    } 
+    {
+    response.write(`A URL é: "${url}".`); // Write a message
+    response.end(); // End the response
+    };
 });
 
 // Runs the server (creates a listen, a wait anyone who wants to enter the server address)
